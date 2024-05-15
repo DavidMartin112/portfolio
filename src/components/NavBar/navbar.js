4c6ddbf9d05eb3c186c9cbdbfacdcdf573350759
DavidMartin112 <martinmorodavid2002@gmail.com>
2024-05-15 15:48:06 +0200

    second commit

---
+++ src/components/NavBar/navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './navbar.css'
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav className='navbar'>
+                <img src='' alt='' />
+                <div className='desktopMenu'>
+                
+                </div>
+                <button className='desktopMenubtn'></button>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
