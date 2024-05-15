1aa3a5daa826793c16e67a5d652b61f086a1e524
DavidMartin112 <martinmorodavid2002@gmail.com>
2024-05-15 22:27:15 +0200

    intro until start of responsiveness

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Navbar from './components/NavBar/navbar';
+import Intro from './components/intro/intro';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Navbar/>
+      <Intro/>
     </div>
   );
 }
---
+++ src/components/intro/intro.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './intro.css';
+import bg from '../../assets/image.png';
+import btnImg from '../../assets/hireme.png';
+import { Link } from 'react-scroll';
+
+const Intro = () => {
+    return (
+        <section id="intro">
+            <div className="introContent">
+                <span className='hello'>Hello,</span>
+                <span className='introText'>I'm <span className='introName'>David</span> <br />Software Developer</span>
+                <p className='introPara'>I am a skilled software developer with experience creating <br /> visually appealing and user friendly websites.</p>
+                <Link><button className='btn'><img src={btnImg} alt='Hire Me' className='btnImg'></img>Hire Me</button></Link>   
+            </div>
+            <img src={bg} alt='Profile' className='bg'/>
+        </section>
+    );
+};
+
+export default Intro;
