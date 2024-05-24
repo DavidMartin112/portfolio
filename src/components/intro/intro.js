f92045ae5f21582aa9ba2a5782cf93cd5978731b
DavidMartin112 <martinmorodavid2002@gmail.com>
2024-05-24 13:30:16 +0200

    Color edit button

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { SketchPicker } from 'react-color';
 import './intro.css';
 import bg from '../../assets/image2.png';
 import btnImg from '../../assets/hireme.png';
+import editImg from '../../assets/edit.png';
 import { Link } from 'react-scroll';
 
 const Intro = () => {
+    const [colorPickerVisible, setColorPickerVisible] = useState(false);
+    const [secondaryColor, setSecondaryColor] = useState('yellow');
+
+    const handleColorChange = (color) => {
+        const root = document.documentElement;
+        root.style.setProperty('--secondary-color', color.hex);
+        setSecondaryColor(color.hex);
+    };
+
     return (
         <section id="intro">
             <div className="introContent">
                 <span className='hello'>Hello,</span>
-                <span className='introText'>I'm <span className='introName'>David</span> <br />Software Developer</span>
+                <span className='introText'>I'm <span className='introName'>
+                    David 
+                    <button className='colorPickerToggle' onClick={() => setColorPickerVisible(!colorPickerVisible)}>
+                        <img src={editImg} alt='Edit' className='editImg'/>
+                    </button>
+                    {colorPickerVisible && (
+                    <div className='colorPicker'>
+                        <SketchPicker
+                            color={secondaryColor}
+                            onChangeComplete={handleColorChange}
+                        />
+                    </div>
+                    )}
+                    </span> <br />Software Developer</span>
                 <p className='introPara'>I am a skilled software developer with experience creating <br /> visually appealing and user friendly websites.</p>
                 <Link><button className='btn'><img src={btnImg} alt='Hire Me' className='btnImg'></img>Hire Me</button></Link>   
             </div>
