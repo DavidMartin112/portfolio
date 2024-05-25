f1556517765397bd222a88236082eaa0ad0df24d
David <martinmorodavid2002@gmail.com>
2024-05-25 12:57:06 +0200

    Update in rights

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -4,7 +4,7 @@ import './footer.css';
 const Footer = () => {
     return (
         <footer id='footer'>
-            Copyright &#169; 2023 Smith Williams. All rights received.
+            This work is dedicated to the public domain by David Martin. No rights reserved.
         </footer>
     );
 };
